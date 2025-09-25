6c5ed91315e1c735e457c92c8d747e04014ea5d0
Mariusz Godlewski <mgodl478@gmail.com>
2025-09-25 13:40:53 +0200

    before AWS

--- MySQLDatabase/Temp.sql
+++ MySQLDatabase/Temp.sql
@@ -20,3 +20,15 @@ TRUNCATE TABLE DayValue;
 TRUNCATE TABLE StockExchangeEntering;
 TRUNCATE TABLE Company;
 TRUNCATE TABLE StockExchange;
+
+
+
+SELECT 
+    table_schema AS `Database`, 
+    ROUND(SUM(data_length + index_length) / 1024 / 1024 / 1024, 2) AS `Size_in_GB`
+FROM 
+    information_schema.tables
+WHERE 
+    table_schema = 'Stock'
+GROUP BY 
+    table_schema;
